f2a4dbe03a25cf914f5f9cf964b805eca9fd597a
Mohamed Achaq <motriyanox@gmail.com>
2023-02-28 16:36:51 +0100

    First Commit

---
+++ pages/annuaire.tsx
@@ -0,0 +1,3 @@
+export default function Annuaire() {
+  return <div>Annuaire</div>;
+}
---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./layouts/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        OpenSans: ["Open Sans", "sans-serif"],
+        NunitoSans: ["Nunito Sans", "sans-serif"],
+        Roboto: ["Roboto", "sans-serif"],
+      },
+      colors: {
+        white: {
+          main: "#FFFFFF",
+          off: "#F5F5F5",
+          cool: "#e6e6e6",
+          dark: "#E0E4EB",
+        },
+        black: {
+          main: "#000000",
+          off: "#15181E",
+          cool: "#1A1A1A",
+        },
+        brand: {
+          main: "#FF8501",
+          mainOff: "#FFB464",
+          mainCool: "#FFF4E5",
+          secondary: "#240046",
+          tertiary: "#442769",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
